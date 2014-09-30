699bbe55ff7a7a22aac056003a1e9fd6a8508b76
Сергей Реймеров <sergey@MacBook-Pro-Sergej.local>
2014-09-30 13:00:24 +0900

    AJAX support first commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var jade = require('jade');
+var fs = require('fs');
+var path = require('path');
 
 var db = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get(/\/_views\/([^.]+)\.jade/, function(req,res) {
+  var filename = path.join(__dirname, '../views', req.params[0], '.jade');
+
+  fs.readFile(filename, function(err, data) {
+    var options = {filename: filename};
+    if (err) throw err;
+
+    res.send("(function() {" + jade.compileClient(data, options) +
+      "; if (!window.Templates) window.Templates = {};" +
+      "window.jades['"+req.params[0]+"'] = template;" +
+      "})();");
+  });
 });
 
 module.exports = router;
