c71dea5d2b965bf1b36a5b5ad62bc5d549899c2d
alekseyboykoJL <kiss.golden@yandex.ru>
2014-11-18 14:09:33 +0400

    Tests added

--- models/TestAnswerOpen.js
+++ models/TestAnswerOpen.js
@@ -2,11 +2,11 @@ module.exports = function(sequelize, DataTypes) {
     var complexRights = {};
     var TestAnswerOpen = sequelize.define('TestAnswerOpen', {
         text:DataTypes.STRING,
-        type:DataTypes.ENUM('auto','script','manual')
+        type:DataTypes.ENUM('auto','manual')
     }, {
         classMethods: {
             associate: function(models) {
-                TestAnswerOpen.hasOne(models.TestQuestion);
+                models.TestQuestion.hasOne(TestAnswerOpen);
             }
         }
     });
